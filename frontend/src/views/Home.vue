afe90ee1f94bed93bf0b81cb09b588fa2ee719cd
rom100main <rom100main@proton.me>
2025-12-17 16:45:47 +0100

    chore(front-end): remove quick links in home

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,12 +1,7 @@
 <template>
   <section>
     <h1>Protein Data Analysis</h1>
-    <p>Frontend for the Protein Data Analysis backend. Quick links:</p>
-    <ul>
-      <li><router-link to="/proteins">Browse proteins</router-link></li>
-      <li><router-link to="/graph/build">Build graph</router-link></li>
-      <li><router-link to="/statistics">View statistics</router-link></li>
-    </ul>
+    <p>Frontend for the Protein Data Analysis backend.</p>
   </section>
 </template>
 
